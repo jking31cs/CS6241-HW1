d0d1c31b2c9ea6232540c8567411e31e55ef20c0
Bobby King <jking31cs@gmail.com>
2014-02-12 17:48:43 -0500

    Added Warshall's Algorithm to the code.

--- Part_3-2.cpp
+++ Part_3-2.cpp
@@ -5,6 +5,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include <list>
 #include <set>
+#include <limits>
 using namespace llvm;
 /*
  * This detects total number of loops, total number of outer loops, and number of back edges via 
@@ -24,6 +25,134 @@ namespace {
     this->U = U;
     this->V = V;
   }
+
+  class Path {
+    public:
+      BasicBlock *U;
+      BasicBlock *V;
+      int dist;
+      Path(BasicBlock*, BasicBlock*);
+      bool operator==(const Path& that) { return this->U == that.U && this->V == that.V; }
+  };
+
+  Path::Path(BasicBlock *U, BasicBlock *V) {
+    this->U = U;
+    this->V = V;
+  }
+
+  std::list<Edge> getLoopInfo(Function *F) {
+    std::list<Edge> edgeList;
+    for (Function::iterator bi = F->begin(), be = F->end(); bi != be; bi++) {
+      BasicBlock *BB = bi;
+      //This allows me to see all paths from BB to other Basic Blocks.
+      for (succ_iterator pi = succ_begin(BB), e = succ_end(BB); pi != e; ++pi) {
+        Edge E(BB, *pi);
+        edgeList.push_back(E);
+      }
+    }
+    return edgeList;
+  }
+
+  bool functionName(Function *F) {
+    errs().write_escaped(F->getName()) << '\n';
+    return false;
+  }
+
+  struct WarshallLoopDetector : public FunctionPass {
+    static char ID;
+    WarshallLoopDetector() : FunctionPass(ID) {}
+    
+    virtual bool runOnFunction(Function &F) {
+      functionName(&F);
+      std::list<Edge> edgeList = getLoopInfo(&F);
+      std::list<Path*> paths;
+      //Initialize the distance between any 2 points to be at max, 0 for distance to itself.
+      errs() << "checkpoint";
+      for (Function::iterator it1 = F.begin(), e1 = F.end(); it1 != e1; it1++) {
+        errs() << "hi";
+        for (Function::iterator it2 = F.begin(), e2 = F.end(); it2 != e2; it2++) {
+          Path path(it1, it2);
+          path.dist = std::numeric_limits<int>::max();
+          errs() << "hello";
+          paths.push_front(&path);
+        }
+      } 
+      for (Function::iterator it1 = F.begin(); it1 != F.end(); it1++) {
+         Path path(it1, it1);
+         for (std::list<Path*>::iterator p = paths.begin(); p != paths.end(); p++) {
+           if ((*p) == &path) {
+             (*p)->dist = 0;
+             
+           }
+         }
+         paths.push_back(&path);
+      }
+      errs() << "checkpoint"; 
+      //Set up edges in the paths
+      for (std::list<Edge>::iterator edge_it = edgeList.begin(), e = edgeList.end(); edge_it != e; ++edge_it) {
+        Path path(edge_it->U, edge_it->V);
+        for (std::list<Path*>::iterator p = paths.begin(); p != paths.end(); p++) {
+          if ((*p) == &path) {
+            (*p)->dist = 1;    
+          }
+        }
+      }
+       errs() << "checkpoint";
+      //Now it's time to find shortest path from all points to every other point.
+      Function::iterator i = F.begin(), e1 = F.end();
+      Function::iterator j = F.begin(), e2 = F.end();
+      Function::iterator k = F.begin(), e3 = F.end();
+      errs() << "Let's find shortest paths guys\n";
+      for (; k != e3; ++k) {
+        for (; i != e1; ++i) {
+          for (; j != e2; ++j) {
+            Path IJ(i, j);
+            Path JK(j, k);
+            Path IK(i, k);
+            for (std::list<Path*>::iterator P = paths.begin(); P != paths.end(); P++) {
+              if ((*P) == &IJ) {
+                IJ = *(*P); 
+              }
+              if ((*P) == &JK) {
+                JK = *(*P);
+                
+              }
+              if ((*P) == &IK) {
+                IK = *(*P);
+              }
+            }
+            
+            if (IK.dist > IJ.dist + IK.dist) {
+              IK.dist = IJ.dist + JK.dist;
+            }
+          }
+        }
+      }
+      //Now loop detection time
+      int numLoops = 0;
+      for (Function::iterator BB1 = F.begin(); BB1 != F.end(); BB1++) {
+        for (Function::iterator BB2 = F.begin(); BB2 != F.end(); BB2++) {
+          if (BB1 == BB2) { continue; }
+          Path *to_path;
+          Path *from_path;
+          for (std::list<Path*>::iterator path = paths.begin(); path != paths.end(); path++) {
+            if ((*path)->U == BB1 && (*path)->V == BB2) {
+              to_path = *path;
+            }
+            if ((*path)->V == BB1 && (*path)->U == BB2) {
+              from_path = *path;
+            }
+          }
+          if (to_path->dist < std::numeric_limits<int>::max() || from_path->dist < std::numeric_limits<int>::max()) {
+            numLoops++;
+          }
+        }
+      }
+      errs() << '\t' << "Num of Loops with Warshall's Alg: " << numLoops << '\n';
+      return false; 
+    }
+  };
+
   struct BackEdgeLoopDetector : public FunctionPass  {
     static char ID;
     BackEdgeLoopDetector() : FunctionPass(ID) {}
@@ -59,25 +188,12 @@ namespace {
       return false;
     }
 
-    std::list<Edge> getLoopInfo(Function *F) {
-      std::list<Edge> edgeList;
-      for (Function::iterator bi = F->begin(), be = F->end(); bi != be; bi++) {
-        BasicBlock *BB = bi;
-        //This allows me to see all paths from BB to other Basic Blocks.
-        for (succ_iterator pi = succ_begin(BB), e = succ_end(BB); pi != e; ++pi) {
-          Edge E(BB, *pi);
-          edgeList.push_back(E);
-        }
-      }
-      return edgeList;
-    };
-
-    bool functionName(Function *F) {
-      errs().write_escaped(F->getName()) << '\n';
-      return false;
-    }
+    
   };
 
   char BackEdgeLoopDetector::ID = 0;
+  char WarshallLoopDetector::ID = 0;
   static RegisterPass<BackEdgeLoopDetector> X("loop-backedge", "Loop Detection via Back edge", false, false);
+  static RegisterPass<WarshallLoopDetector> X1("loop-warshall", "Loop Detection via Warshall's Alg", false, false);
+
 }
